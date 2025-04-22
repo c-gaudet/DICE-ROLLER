0fbdae66b764d3e85c49ef5ce3b3e5c907b88bad
c-gaudet <c.gaudet.anim@gmail.com>
2025-04-21 23:30:20 -0400

    plus 1
    i goofed

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let num;
 
 
 document.getElementById("rollbutton").onclick = function(){
-    num = Math.floor(Math.random()*20);
+    num = Math.floor(Math.random()*20) + 1;
     document.getElementById("rollresult").innerHTML = num;
 }
